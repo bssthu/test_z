4616bcbcbfc89e12aaf7698f172a680449994f0a
bssthu <bssthu@users.noreply.github.com>
2015-08-09 12:21:56 +0800

    add decoder class

---
+++ eso_zh_server/QrDecoder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing;
+using ZXing.Common;
+
+namespace eso_zh_server
+{
+    static class QrDecoder
+    {
+        public static String MultiDecode(Bitmap bitmap)
+        {
+            // convert
+            if (bitmap == null)
+            {
+                return null;
+            }
+            LuminanceSource luminanceSource = new BitmapLuminanceSource(bitmap);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
+
+            // decode
+            ZXing.Result result = (new MultiFormatReader()).decode(binaryBitmap);
+            if (result == null)
+            {
+                return null;
+            }
+
+            String text = result.Text;
+            return text;
+        }
+    }
+}
