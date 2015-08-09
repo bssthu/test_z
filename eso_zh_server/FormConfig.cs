da3101127ac04281c73362eaa7a6592b0732cc6d
bssthu <bssthu@users.noreply.github.com>
2015-08-09 13:21:53 +0800

    add worker thread (empty)

--- eso_zh_server/FormConfig.cs
+++ eso_zh_server/FormConfig.cs
@@ -5,15 +5,37 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace eso_zh_server
 {
     public partial class FormConfig : Form
     {
+        private Worker worker;
+        private Thread workerThread;
+
         public FormConfig()
         {
             InitializeComponent();
+            worker = new Worker(this);
+            workerThread = new Thread(worker.Run);
+            workerThread.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // kill worker thread
+            if (workerThread != null)
+            {
+                workerThread.Interrupt();
+                while (workerThread.IsAlive)
+                {
+                    Thread.Sleep(200);
+                }
+            }
+
+            base.OnClosing(e);
         }
     }
 }
---
+++ eso_zh_server/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace eso_zh_server
+{
+    class Worker
+    {
+        FormConfig frm;
+
+        public Worker(FormConfig frm)
+        {
+            this.frm = frm;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch (ThreadInterruptedException) // ok
+                {
+                    return;
+                }
+                // do work
+            }
+        }
+    }
+}
